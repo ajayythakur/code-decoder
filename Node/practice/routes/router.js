3074ac81522170af306c785823f44d106e5e26b2
ajayythakur <ajay1270panwar@gmail.com>
2023-09-26 18:18:14 +0530

    code decoder task of copy.ai

--- Node/practice/routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const register = require("../controller/registerController");
-const login = require("../controller/loginController");
-const router = express.Router();
-
-router.post("/register",register);
-router.post("/login",login);
-module.exports = router;
